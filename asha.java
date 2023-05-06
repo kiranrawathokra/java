45bc2b9df173ac0057aec417afca25b3ed3aead3
kiranrawathokra <kiranrawatsoftdev@gmail.com>
2023-05-06 11:14:32 +0530

    assigement

---
+++ asha.java
@@ -0,0 +1,8 @@
+class Main{
+    public static void main(String arr[])
+    {
+        int length=10,breath=20,area;
+        area=length*breath;
+        System.out.println("the area of arectangle is"+area);
+    }
+}
